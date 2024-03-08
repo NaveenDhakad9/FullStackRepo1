00048c71e95372bdd5255fe2fecf8e8979f78f49
patid <patid@DESKTOP-QBMGQ95>
2024-03-08 17:55:43 +0530

    Task1 From dev1

--- GitProj01/src/main/java/com/nt/main/AppTest.java
+++ GitProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,9 +1,12 @@
 package com.nt.main;
+import com.nt.service.ArthmeticOperations;
 
 public class AppTest 
 {
     public static void main(String args[])
     {
-    	
+    	ArthmeticOperations ar = new ArthmeticOperations();
+    	System.out.println("sum : "+ ar.sum(100,200));
+        System.out.println("sub : "+ ar.sub(100, 200));
     }
 }
